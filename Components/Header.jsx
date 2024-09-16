3edc29996d795a5e3bd8402619312162ce0475ef
Ramesh Dhayal <rameshdhayalrd01@gmail.com>
2024-09-16 12:59:03 +0530

    d

--- Components/Header.jsx
+++ Components/Header.jsx
@@ -1,26 +1,22 @@
-import { useTheme } from "../Hooks/useTheme";
+import { useTheme } from "../Hooks/useTheme"
 
 export default function Header() {
-  const [isDark, setIsDark] = useTheme();
-
+  const [isDark, setIsDark] =  useTheme()
+  
   return (
-    <header className={`header-container ${isDark ? "dark" : ""}`}>
+    <header className={`header-container ${isDark? 'dark': ''}`}>
       <div className="header-content">
         <h2 className="title">
-          <a href="/">Where in the worlds?</a>
+          <a href="/">Where in the world?</a>
         </h2>
-
-        <p
-          className="theme-changer"
-          onClick={() => {
-            setIsDark(!isDark);
-            localStorage.setItem("isDarkMode", !isDark);
-          }}
-        >
-          <i className={`fa-solid fa-${isDark ? "sun" : "moon"}`} />
-          &nbsp;&nbsp;{isDark ? "Light" : "Dark"} Mode
+        <p className="theme-changer" onClick={() => {
+          setIsDark(!isDark)
+          localStorage.setItem('isDarkMode', !isDark)
+        }}>
+          <i className={`fa-solid fa-${isDark ? 'sun': 'moon'}` }/>
+          &nbsp;&nbsp;{isDark? 'Light': 'Dark'} Mode
         </p>
       </div>
     </header>
-  );
+  )
 }
